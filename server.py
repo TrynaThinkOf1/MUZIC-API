e777a1ee0c3063b558ec2e176b6a2f15e19f2ca4
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-12-20 16:06:46 -0500

    "{'message': "Failed to decode JSON object: 'utf-8' codec can't decode byte 0x82 in position 253: invalid start byte"}"

--- server.py
+++ server.py
@@ -6,11 +6,14 @@
 
 #########################
 # IMPORT EXT. LIBRARIES
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, marshal_with, reqparse, fields, abort
 from flask_sqlalchemy import SQLAlchemy
 # IMPORT INCL. LIBRARIES
-import secrets, hashlib
+import secrets, hashlib, os
+
+from werkzeug.utils import send_file
+
 # IMPORT LOCAL LIBRARIES
 #########################
 
@@ -20,6 +23,9 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases.db'
 db = SQLAlchemy(app)
+
+UPLOAD_FOLDER = 'songs'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 #########################
 
 #########################
@@ -36,13 +42,15 @@ class SongDB(db.Model):
 
 class Keys(db.Model):
     key = db.Column(db.String, unique=True, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
     admin_key = db.Column(db.String, unique=True)
 #########################
 
 #########################
 #    ARGS & PARSERS
 song_get_args = reqparse.RequestParser()
-song_get_args.add_argument('key', type=str, required=True)
+#song_get_args.add_argument('key', type=str, required=True)
 song_get_args.add_argument('song_id', type=int)
 
 song_post_args = reqparse.RequestParser()
@@ -55,13 +63,21 @@ song_patch_args.add_argument('gov_name', type=str, required=True)
 
 song_del_args = reqparse.RequestParser()
 song_del_args.add_argument('admin_key', type=str, required=True)
+
+key_post_args = reqparse.RequestParser()
+key_post_args.add_argument('name', type=str, required=True)
 #########################
 
 #########################
 #      DECORATORS
 field_flavors = {
     "song_id": fields.Integer,
-    "raw_key": fields.String
+    "gov_name": fields.String,
+    "nick_name": fields.String,
+    "writer_name": fields.String,
+
+    "raw_key": fields.String,
+    "name": fields.String
 }
 #########################
 
@@ -91,7 +107,7 @@ def fileAllowed(file):
 
 #########################
 #       RESOURCES
-class SongResource(Resource):
+class GetSong(Resource):
     @marshal_with(field_flavors)
     def get(self):
         args = song_get_args.parse_args()
@@ -99,30 +115,47 @@ class SongResource(Resource):
         if not song:
             abort(404, message="Song not found")
 
-        if hashKey(args['key']) not in Keys.query.all():
-            abort(404, message="Key not valid")
+        #if hashKey(args['key']) not in Keys.query.all():
+            #abort(404, message="Key not valid")
 
-        return song
+        #if not os.path.exists(song.path):
+            #return jsonify({"message": "File not found"}), 404
 
-    # ¡ ADMINS ONLY !
+        return song #send_file(song.path, mimetype="audio/mpeg")
+
+# ¡ ADMINS ONLY !
+class PostSong(Resource):
     @marshal_with(field_flavors)
     def post(self):
         args = song_post_args.parse_args()
         if not Keys.query.filter_by(admin_key=hashKey(args['admin_key'])).first():
             abort(403, message="You're not allowed to do that, silly billy!")
 
+        # FILE STUFF
+        if 'file' not in request.files:
+            return jsonify({"message": "No file part"}), 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"message": "No selected file"}), 400
+
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        #########
+
         id = getNextSongID()
         gov_name = args['gov_name']
         nick_name = extract(args['gov_name'], 'nick_name')
         writer_name = extract(args['gov_name'],'writer_name')
-        path = AHAHAHAHAHHAHFBJSAUFTDIUAKYGJHBKWDVUYFO8AGILU BHDGIAGKWJDBSVCUYO7TOYAIHLWJDS GCILAUWYO87ILUWAKYEH BD
 
-        song = SongDB(id=id, gov_name=gov_name, nick_name=nick_name, writer_name=writer_name, path=path)
+        song = SongDB(id=id, gov_name=gov_name, nick_name=nick_name, writer_name=writer_name, path=filepath)
         db.session.add(song)
         db.session.commit()
 
         return song, 201
 
+class PatchSong(Resource):
     @marshal_with(field_flavors)
     def patch(self):
         args = song_patch_args.parse_args()
@@ -142,6 +175,7 @@ class SongResource(Resource):
 
         return song, 200
 
+class DeleteSong(Resource):
     def delete(self):
         args = song_del_args.parse_args()
         if not Keys.query.filter_by(admin_key=hashKey(args['admin_key'])).first():
@@ -161,9 +195,10 @@ class KeyResource(Resource):
     def post(self):
         raw_key = secrets.token_hex(32)
         hashed_key = hashKey(raw_key)
+        args = key_post_args.parse_args()
 
         if not Keys.query.filter_by(key=hashed_key).first():
-            new_key = Keys(key=hashed_key)
+            new_key = Keys(key=hashed_key, name=args['name'])
             db.session.add(new_key)
             db.session.commit()
 
@@ -173,8 +208,12 @@ class KeyResource(Resource):
 
 #########################
 #       ENDPOINTS
-api.add_resource(SongResource, '/song')
-api.add_resource(KeyResource, '/key')
+api.add_resource(GetSong, '/song/get')
+api.add_resource(PostSong, '/song/post')
+api.add_resource(PatchSong, '/song/patch')
+api.add_resource(DeleteSong, '/song/delete')
+
+api.add_resource(KeyResource, '/key/create')
 #########################
 
 #########################
@@ -183,6 +222,7 @@ api.add_resource(KeyResource, '/key')
 
 #########################
 if __name__ == "__main__":
-    with app.app_context(): # UNCOMMENT ON INITIAL CREATION/RECREATION
-        db.create_all()
+    #with app.app_context(): # UNCOMMENT ON INITIAL CREATION/RECREATION
+        #db.drop_all()
+        #db.create_all()
     app.run(debug=True, host="0.0.0.0", port=6969) # ¡¡¡ DON'T RUN ON DEBUG WHEN IN PRODUCTION !!!
--- tests/test1.py
+++ tests/test1.py
@@ -1,1 +1,10 @@
-THIS SHIT ISNT FUCKING WORKING
+import requests
+
+url = "http://127.0.0.1:6969/key/create"
+admin_key = "e5011778269baedc706ef559fd13f2443c8b78ffa7f816d5288d975e185f780d"
+file_path = "../_bell1.mp3"
+
+with open(file_path, "rb") as f:
+    files = {"file": f}
+    response = requests.post(url, headers={"Content-Type": "application/json"}, files=files, json={"key": admin_key, "gov_name": "_bell1-iMovie Audio Effects Library"})
+    print(response.json())
